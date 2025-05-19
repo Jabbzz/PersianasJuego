80ededc929b6e52134a132cf4dd8ae3d0d65c131
Chocolatitu <125135426+Chocolatitu@users.noreply.github.com>
2025-05-19 13:27:48 +0200

    Musica de ambiente

--- Assets/Scripts/audioManager.cs
+++ Assets/Scripts/audioManager.cs
@@ -4,6 +4,9 @@ public class audioManager : MonoBehaviour
 {
     public static audioManager instance;  // Singleton instance
     public AudioSource soundEffectSource; // Drag an AudioSource here in the Inspector
+    public AudioSource musicSource;
+
+    public AudioClip ambientMusic;
 
     private void Awake()
     {
@@ -21,8 +24,28 @@ public class audioManager : MonoBehaviour
         DontDestroyOnLoad(gameObject); // Keep the AudioManager across scenes
     }
 
+    private void Start()
+    {
+        PlayMusic(ambientMusic); 
+    }
+
     public void PlaySound(AudioClip clip)
     {
         soundEffectSource.PlayOneShot(clip); // Play a one-time sound
+    }
+
+    public void PlayMusic(AudioClip musicClip, bool loop = true)
+    {
+        if (musicSource.clip != musicClip)
+        {
+            musicSource.clip = musicClip;
+            musicSource.loop = loop;
+            musicSource.Play();
+        }
+    }
+
+    public void StopMusic()
+    {
+        musicSource.Stop();
     }
 }
